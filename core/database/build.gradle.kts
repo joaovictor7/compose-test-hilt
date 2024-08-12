77e42239ea2ad4044296b4ff3e2b2a5cefdd82f5
joao.matos <joaovictormatos705@gmail.com>
2024-08-12 10:03:30 +0100

    remove data pro store dependency from database module

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -10,7 +10,6 @@ dependencies {
     implementation(projects.core.security)
     implementation(projects.core.domain)
     implementation(projects.common)
-    implementation(libs.androidx.dataStore)
     implementation(libs.room)
     ksp(libs.room.compile)
 }
