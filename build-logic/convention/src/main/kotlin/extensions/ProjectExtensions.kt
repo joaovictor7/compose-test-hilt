1a05d4893ac48a05dda4344d8dfc25a999cb561e
joao.matos <joaovictormatos705@gmail.com>
2024-12-09 16:27:28 +0000

    change files properties optional

--- build-logic/convention/src/main/kotlin/extensions/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/extensions/ProjectExtensions.kt
@@ -4,19 +4,16 @@ import enums.File
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
-import java.io.FileNotFoundException
 import java.util.Properties
 
 internal fun Project.getLibrary(id: String) =
     extensions.getByType<VersionCatalogsExtension>().named("libs").findLibrary(id).get()
 
-internal fun Project.loadPropertiesFile(file: File): Properties {
-    val propertiesFile = file(file.path)
-    return Properties().apply {
-        if (propertiesFile.exists()) {
-            propertiesFile.inputStream().use { load(it) }
-        } else {
-            throw FileNotFoundException("File '${file.path}' not found.")
-        }
+internal fun Project.loadPropertiesFile(file: File) = file(file.path).let {
+    if (it.exists()) {
+        Properties().apply { load(it.inputStream()) }
+    } else {
+        println("File '${file.path}' not found.")
+        null
     }
 }
--- build-logic/convention/src/main/kotlin/modularization/BuildConfigFields.kt
+++ build-logic/convention/src/main/kotlin/modularization/BuildConfigFields.kt
@@ -16,17 +16,14 @@ internal fun ApplicationDefaultConfig.setDefaultBuildConfigFields(project: Proje
         buildConfigField("String", "BFF_API_HOST", "\"\"") // Macbook
 //    buildConfigField("String", "BFF_API_HOST", "\"10.0.2.2\"") // Emulator
         buildConfigField("int", "BFF_API_PORT", "0")
-        buildConfigField("String", "NEWS_API_API_KEY", properties.getProperty("NEWS_API"))
+        buildConfigField("String", "NEWS_API_API_KEY", properties?.getProperty("NEWS_API").orEmpty())
     }
 
 internal fun ApplicationBuildType.setBuildConfigFields(
     project: Project,
     buildType: BuildType
 ) = with(project) {
-    when (buildType) {
-        BuildType.RELEASE -> {}
-        BuildType.DEBUG -> {}
-    }
+
 }
 
 internal fun ApplicationProductFlavor.setBuildConfigFields(
@@ -34,11 +31,5 @@ internal fun ApplicationProductFlavor.setBuildConfigFields(
     dimension: FlavorDimension,
     flavor: Flavor
 ) = with(project) {
-    when (flavor) {
-        Flavor.FREE -> {}
-        Flavor.FULL -> {}
-        Flavor.PRODUCTION -> {}
-        Flavor.DEVELOP -> {}
-        Flavor.STAGING -> {}
-    }
+
 }
