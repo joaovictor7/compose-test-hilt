61e4d2f9b0c4aee9ca0d736fe84a5416e0c29add
joao.victor <joaovictormatos705@gmsil.com>
2025-02-08 22:52:52 +0000

    module exchange created

---
+++ feature/exchange/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.composeTest.library)
+    alias(libs.plugins.composeTest.compose)
+    alias(libs.plugins.composeTest.test)
+}
+
+android {
+    namespace = "com.composetest.feature.exchange"
+}
+
+dependencies {
+    implementation(projects.common)
+    implementation(projects.core.router)
+    implementation(projects.core.designsystem)
+    implementation(projects.core.domain)
+    implementation(projects.core.ui)
+}
